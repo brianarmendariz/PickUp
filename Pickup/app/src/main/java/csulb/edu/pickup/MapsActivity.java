40bc0cc6fadbbcecee3360a2985892696b5b8628
Nessa <Dao_ri_zan@hotmail.com>
2016-02-15 16:13:38 -0800

    Added Longtitude and Latitude

--- Pickup/app/src/main/java/csulb/edu/pickup/MapsActivity.java
+++ Pickup/app/src/main/java/csulb/edu/pickup/MapsActivity.java
@@ -9,9 +9,12 @@ import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -28,6 +31,30 @@ public class MapsActivity extends FragmentActivity {
         map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
         // Enabling MyLocation Layer of Google Map
         map.setMyLocationEnabled(true);
+        map.setOnMapClickListener(new OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng point) {
+                // Drawing marker on the map
+                drawMarker(point);
+
+                Log.d(String.valueOf(point.longitude), "this is longtitude");
+                Log.d(String.valueOf(point.latitude), "this is latitude");
+                Toast.makeText(getBaseContext(), "Longtitude: " + String.valueOf(point.longitude) + "\n" +
+                        "Latitude: " + String.valueOf(point.latitude), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+    private void drawMarker(LatLng point){
+        // Creating an instance of MarkerOptions
+        MarkerOptions markerOptions = new MarkerOptions();
+
+        // Setting latitude and longitude for the marker
+        markerOptions.position(point);
+
+        // Adding marker on the Google Map
+        map.addMarker(markerOptions);
     }
 
     @Override
