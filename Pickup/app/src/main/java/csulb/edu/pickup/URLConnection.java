90909ad77c0d99b5cdb01ec72beba99d0e99bdad
brianarmendariz <brian.armendariz@student.csulb.edu>
2016-10-09 20:51:45 -0700

    Fix to sendCreateEvent method call

--- Pickup/app/src/main/java/csulb/edu/pickup/URLConnection.java
+++ Pickup/app/src/main/java/csulb/edu/pickup/URLConnection.java
@@ -305,9 +305,9 @@ public class URLConnection
      */
     public String sendCreateEvent( String authorName,String authorEmail, String eventName, String sport,
                                    String location, String latitude, String longitude, String eventDateTime, String ageMax, String ageMin,
-                                   String minUserRating, String playerAmount, String isPrivate, String gender, String Skill, String SportSpecific,
-                                   String EventEndDateTime, String PlayersPerTeam, String NumberOfTeams, String Terrain,
-                                   String Environment) throws IOException  {
+                                   String minUserRating, String playerAmount, String isPrivate, String gender, String skill, String sportSpecific,
+                                   String eventEndDateTime, String playersPerTeam, String numberOfTeams, String terrain,
+                                   String environment) throws IOException  {
 
 				/*url of route being requested*/
         String url = "http://www.csulbpickup.com/createEvent.php";
@@ -315,8 +315,8 @@ public class URLConnection
         String urlParameters = "AuthorName="+authorName+"&Email="+authorEmail+"&EventName="+eventName+"&Sport="+sport+"&Location="+location+"&Latitude="+latitude+
                 "&Longitude="+longitude+"&EventDateTime="+eventDateTime+"&AgeMax="+ageMax+"&AgeMin="+ageMin+
                 "&PlayerAmount="+playerAmount+"&MinUserRating="+minUserRating+"&IsPrivate="+isPrivate+"&Gender="+gender+
-                "&Skill="+Skill+"&SportSpecific="+SportSpecific+"&EventEndDateTime="+EventEndDateTime+"&EventEndDate="+EventEndDate+"&PlayersPerTeam="+PlayersPerTeam+"&PlayersPerTeam="+PlayersPerTeam+
-                "&Terrain="+Terrain+"&Environment="+Environment;
+                "&Skill="+skill+"&SportSpecific="+sportSpecific+"&EventEndDateTime="+eventEndDateTime+"&PlayersPerTeam="+playersPerTeam+"&NumberOfTeams="+numberOfTeams+
+                "&Terrain="+terrain+"&Environment="+environment;
 
         return makeHTTPRequest(url,urlParameters);
 
